ee2a5ec94fd032a2f04b81c20aa17bb4bac734ba
Sonny Shi <thisisshi@gmail.com>
2016-04-07 22:29:41 -0400

    better human readable times with moment.js

--- client/tasks/Task.js
+++ client/tasks/Task.js
@@ -3,3 +3,14 @@ Template.Task.events({
     Meteor.call('deleteTask', this._id);
   }
 })
+Template.Task.helpers({
+    momentFormate: function(time) {
+
+        if ((moment().unix() - moment(time).unix()) < 3600) {
+            return moment(time).fromNow();
+        } else {
+            return moment(time).format("YYYY-MM-DD HH:mm");
+        }
+    },
+
+    })
