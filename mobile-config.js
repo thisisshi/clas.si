55062534b0e2f71f90cb0517409ee216dc49d081
Sonny <thisisshi@gmail.com>
2016-04-09 20:05:10 -0400

    added mobile config

---
+++ mobile-config.js
@@ -0,0 +1,23 @@
+// This section sets up some basic app metadata,
+// the entire section is optional.
+App.info({
+  id: 'com.classi.classi.android',
+  name: 'clas.si',
+  description: 'A Task Management Application for Students, by Students',
+  author: 'Sonny Shi',
+  email: 'theli@outlook.com',
+  website: 'thisisshi.github.io'
+});
+
+// Set up resources such as icons and launch screens.
+App.icons({
+  // ... more screen sizes and platforms ...
+});
+
+App.launchScreens({
+  // ... more screen sizes and platforms ...
+});
+
+// Set PhoneGap/Cordova preferences
+
+// Pass preferences for a particular PhoneGap/Cordova plugi
